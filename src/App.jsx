25eca0d71db528d6d8661d56bac6ca4dd8e97f61
Npx <cheyprivado@gmail.com>
2024-01-21 14:53:48 -0300

    🎨 Atualizando estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from "./assets/logo.svg";
+import menuIcon from "./assets/icon-menu.svg";
+import menuCloseIcon from "./assets/icon-menu-close.svg";
+import { MenuItem } from "./components/MenuItem";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  function openMenu() {
+    document.querySelector(".container").classList.add("menu-expanded");
+  }
+
+  function closeMenu() {
+    document.querySelector(".container").classList.remove("menu-expanded");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container">
+      <nav className="navigationbar">
+        <img src={logo} alt="Logo" />
+        <div className="menu">
+          <ul className="menu-list">
+            <MenuItem name="Home" />
+            <MenuItem name="New" />
+            <MenuItem name="Popular" />
+            <MenuItem name="Trending" />
+          </ul>
+        </div>
+        <button className="open-menu-button" onClick={openMenu}>
+          <img className="menu-icon" src={menuIcon} alt="Open Icon" />
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <button className="close-menu-button" onClick={closeMenu}>
+          <img
+            className="menu-icon"
+            src={menuCloseIcon}
+            alt="Menu Close Icon"
+          />
+        </button>
+      </nav>
+    </div>
+  );
 }
-
-export default App
